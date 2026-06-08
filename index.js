86a0280dfc0f22c244d5d9522c0f432d380a922e
RaySan14 <102859833+RaySan14@users.noreply.github.com>
2026-06-08 12:01:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder
+} = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
   console.log(`✅ ${client.user.tag} online!`);
+
+  const channel = await client.channels.fetch('ISI_CHANNEL_ID_DISINI');
+
+  const embed = new EmbedBuilder()
+    .setColor('#5865F2')
+    .setTitle('📍 Domisili Catalog')
+    .setDescription(`
+Silahkan pilih roles sesuai dengan domisili asal kamu!
+
+🌴 | Bali
+🌿 | Banten
+🏙️ | Jakarta
+🌄 | Jawa Barat
+🏛️ | Jawa Tengah
+🌋 | Jawa Timur
+🌳 | Kalimantan
+🏝️ | Nusa Tenggara
+🦜 | Papua
+🐟 | Sulawesi
+🌴 | Sumatra
+🎭 | Yogyakarta
+🌍 | International
+`)
+    .setFooter({
+      text: 'Click menu ini untuk memilih roles!'
+    });
+
+  await channel.send({
+    embeds: [embed]
+  });
 });
 
 client.login(process.env.TOKEN);
